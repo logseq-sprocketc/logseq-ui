21979db10738d9a3d39ef59e84939f39cdfe578c
Konstantinos Kaloutas <konstantinos@logseq.com>
2022-10-24 15:40:02 +0300

    add accessibility tests

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,7 +8,8 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    'storybook-dark-mode'
+    "@storybook/addon-a11y",
+    "storybook-dark-mode"
   ],
   framework: "@storybook/react",
   core: {
